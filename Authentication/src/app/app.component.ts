fcfe05047501f3db21fa413f8c8bc59712e6579f
Akansha Wajge <akanshad.wajge@gmail.com>
2019-10-21 14:12:45 +0530

    hide the nav for unauthenticated users
    update the main nav to include a logout link, and hide the nav for unauthenticated users.

--- Authentication/src/app/app.component.ts
+++ Authentication/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import { AuthenticationService } from './_services';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
-  title = 'Authentication';
+    currentUser: any;
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    }
+
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
 }
+
+/*
+The app component uses the authentication service to know the current logged in status and to implement logout.
+
+The currentUser property is used to show/hide the nav when the user is logged in/out. 
+The constructor() method subscribes to the this.authenticationService.currentUser observable and updates 
+the currentUser when the user logs in/out.
+
+The logout() method calls this.authenticationService.logout(); to log the user out, then redirects to the login page. 
+*/
