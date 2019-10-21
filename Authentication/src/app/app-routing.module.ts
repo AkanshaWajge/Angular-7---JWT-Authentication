60a748cf6166aadaab5e1a3c62163917adebc897
Akansha Wajge <akanshad.wajge@gmail.com>
2019-10-21 14:23:40 +0530

    Add Auth Guard to Home Page Route
    To control access to a route with the auth guard you add it to the canActivate array in the route's configuration. The route guards in the canActivate array are run by Angular to decide if the route can be "activated", if all of the route guards return true navigation is allowed to continue, but if any of them return false navigation is cancelled.

--- Authentication/src/app/app-routing.module.ts
+++ Authentication/src/app/app-routing.module.ts
@@ -1,21 +1,28 @@
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AuthGuard } from './_helpers';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
 
-  // otherwise redirect to home
-  { path: '**', redirectTo: '' }
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const appRoutingModule = RouterModule.forRoot(routes);
+
+/* 
+To control access to a route with the auth guard you add it to the canActivate array in the route's configuration.
+ The route guards in the canActivate array are run by Angular to decide if the route can be "activated", if all of
+  the route guards return true navigation is allowed to continue, but if any of them return false navigation is cancelled.
+
+We'll be adding the auth guard to home page route so users so users will have to be logged in to see the home page.
+
+Open the app routing module file (/src/app/app.routing.ts) and add canActivate: [AuthGuard] to the home page (HomeComponent) 
+route.
+*/
